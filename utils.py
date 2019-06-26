02f48867f159b046f84b62484f2554db9aa3b570
Hung Kim Chau <hungkimc@zillowgroup.com>
2019-06-26 14:47:50 -0700

    add read_csv function

--- utils.py
+++ utils.py
@@ -15,3 +15,28 @@ def report2dict(cr):
             print(r.split())
             df.loc[df.shape[0]] = r.split()[-5:]
     return df
+
+
+def read_csv(input_file):
+    """ Deal with reading big csv files """
+    with open(input_file, 'r') as f:
+        a = f.readline()
+    csv_list = a.split(',')
+    tsv_list = a.split('\t')
+    if len(csv_list) > len(tsv_list):
+        sep = ','
+    else:
+        sep = '\t'
+
+    reader = pd.read_csv(input_file, iterator=True, low_memory=False, delimiter=sep)
+    loop = True
+    chunk_size = 100000
+    chunks = []
+    while loop:
+        try:
+            chunk = reader.get_chunk(chunk_size)
+            chunks.append(chunk)
+        except StopIteration:
+            loop = False
+    df = pd.concat(chunks, ignore_index=True)
+    return df
