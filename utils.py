0c4375689ddf4ccb4f1497e6077d5a997a6378ef
Hung Kim Chau <hungkimc@zillowgroup.com>
2019-06-26 14:45:52 -0700

    update comments

--- utils.py
+++ utils.py
@@ -1,9 +1,15 @@
 import pandas as pd
 
+
 def report2dict(cr):
+    """
+    This function is to transform the free text result from classification_result to indexed format dataframe
+    :param cr: the text result from classification_result()
+    :return: dataframe format for the result
+    """
     # Parse rows
     rows = cr.split("\n")
-    df = pd.DataFrame(columns=['metric']+rows[0].split())
+    df = pd.DataFrame(columns=['metric'] + rows[0].split())
     for r in rows[1:]:
         if r != '':
             print(r.split())
